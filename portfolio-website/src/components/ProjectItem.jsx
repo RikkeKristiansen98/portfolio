f0d1338ac6c34f959bc7cf63ecdbe3a97be883ce
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-02-29 20:41:42 +0100

    added education, project sections. some styling

---
+++ portfolio-website/src/components/ProjectItem.jsx
@@ -0,0 +1,8 @@
+export const ProjectItem = () => {
+    return (
+        <div>
+<a href="https://github.com/RikkeKristiansen98" className="text-white bg-[#001b5e] hover:bg-bg-[#001b5e] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#001b5e] dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Github</a>
+
+        </div>
+    )
+}
--- portfolio-website/src/components/Sidenav.jsx
+++ portfolio-website/src/components/Sidenav.jsx
@@ -25,11 +25,11 @@ return (
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
     </a>
-    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+    <a href="#project" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
     </a>
-    <a href="#work" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+    <a href="#education" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
     </a>
@@ -50,7 +50,7 @@ return (
                         <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <AiOutlineHome size={20}/>
                         </a>
-                        <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <a href="#education" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <GrProjects size={20}/>
                         </a>
                         <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
