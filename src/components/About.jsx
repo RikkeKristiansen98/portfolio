70324c7900bb6cd92db6618ef9ce3e04c940e28f
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-03-01 11:40:28 +0100

    About page started

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+
+
+export const About = () => {
+    return (
+        <div id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+<h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">Vem är jag?</h1>
+        </div>
+    )
+}
